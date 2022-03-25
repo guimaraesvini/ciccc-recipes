afbff1d089381309aa33bdaf04d5b90787539151
Yuto Yamakita <yuvto.0420@gmail.com>
2022-03-25 14:40:07 -0700

    Transform var to let

--- script/recipe.js
+++ script/recipe.js
@@ -1,12 +1,12 @@
 const tabs=document.querySelectorAll("[data-tab-target]")
 const tabContents=document.querySelectorAll("[data-tab-content]")
 
-// const tabChange = () =>{
-// }
+
 
 tabs.forEach(tab=>{
     tab.addEventListener("click",() =>{
-        const target=document.querySelector(tab.dataset.tabTarget)
+        const target=document.querySelector(tab.dataset.tabTarget);
+
         tabContents.forEach(tabContent=>
             {tabContent.classList.remove("active");
         })
@@ -22,7 +22,7 @@ tabs.forEach(tab=>{
 })
 
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -36,9 +36,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
